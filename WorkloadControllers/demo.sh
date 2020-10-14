d29426f85197b152dc9e4ca7c7fd27a53b7d7475
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-10-14 18:42:04 +0000

    Scripted demo of Workload Controllers

---
+++ WorkloadControllers/demo.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+press() {
+    echo "$*"
+    echo "Press <return>"
+    read DUMMY
+    [ "$DUMMY" = "q" ] && exit 0
+    [ "$DUMMY" = "Q" ] && exit 0
+}
+
+RUN() {
+    echo; press "-- $*"
+    $*
+}
+
+echo "In other window: run command" 
+press "watch kubectl get dep,ds,rs,pod,sts,job,cj"
+
+SHOW_TYPE() {
+    KIND=$1; shift
+    YAMLS=$*
+
+    echo; echo "-- ${KIND}:";
+    for YAML in $YAMLS; do
+        RUN cat $YAML
+        RUN kubectl create -f $YAML
+        RUN kubectl delete -f $YAML
+    done
+}
+
+RUN kubectl delete -f ./
+
+SHOW_TYPE "Deployment" deploy_ckad.yaml
+# SHOW_TYPE "ReplicaSet (alone)" rset_ckad.yaml
+
+SHOW_TYPE "DaemonSet" dset_ckad.yaml
+
+SHOW_TYPE "StatefulSet (show scale down/delete)" sset_ckad.yaml
+RUN kubectl apply -f svc_sset_ckad.yaml
+RUN kubectl apply -f sset_ckad_v2.yaml
+RUN kubectl scale sts ckad-demo-sset --replicas=0
+
+# TODO: Use of successJobHistory / failedJobHistory / what about maximum time?
+# TODO: Show Completed state
+SHOW_TYPE "Job"      job_ckad.yaml
+SHOW_TYPE "Cron job" cronjob_ckad.yaml
+
+RUN kubectl delete -f ./
+
