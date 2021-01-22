05676817740b3a7b1caec9450a0f86ba73e8703f
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-01-22 09:23:58 +0000

    Facilitate pod selection

--- ephemeral-containers/show_ephemeral_containers.sh
+++ ephemeral-containers/show_ephemeral_containers.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-POD_SPEC="-n kube-system etcd-master"
+# -- Args: --------------------------------------------
+
+POD_SPEC="-l app=debug-ectest"
+
 [ ! -z "$1" ] && POD_SPEC="$*"
 
-kubectl get pod $POD_SPEC -o json | jq -c '.spec.ephemeralContainers[] | { name, command }'
+[ "$POD_SPEC" = "etcd" ] && POD_SPEC="-n kube-system etcd-master";
+
+# -- Main: --------------------------------------------
+
+echo "kubectl get pod $POD_SPEC -o json"
+if [ "${POD_SPEC#-l}" != "$POD_SPEC" ]; then
+    kubectl get pod $POD_SPEC -o json | jq -C -c '.items[].spec.ephemeralContainers[] | { name, command }' | sed 's/^/  /'
+else
+    kubectl get pod $POD_SPEC -o json | jq -C -c '.spec.ephemeralContainers[] | { name, command }'         | sed 's/^/  /'
+fi
 
