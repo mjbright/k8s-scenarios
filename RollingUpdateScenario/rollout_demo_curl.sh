10d106c65616116e4aa95f7dad1a248bf544560b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-05-18 13:16:59 +0200

    Updated rollout_demo_curl.sh to use wget! from a testpod
    Can be useful in case of cluster routing problems!

--- RollingUpdateScenario/rollout_demo_curl.sh
+++ RollingUpdateScenario/rollout_demo_curl.sh
@@ -11,14 +11,24 @@ GET_SVC_IP() {
 GET_SVC_IP
 
 CURL_OPTS="--connect-timeout 2 --max-time 4"
+WGET_OPTS="--timeout 4 --tries 1"
 
-while true; do
-    if [ -z "$IP" ]; then
-        GET_SVC_IP
-    else
-        curl $CURL_OPTS ${IP}:80/1;
-        [ $? -ne 0 ] && GET_SVC_IP
-    fi
-    sleep 1;
-done
+LOOP_CONNECT_FROM_LOCAL_NODE() {
+    while true; do
+        if [ -z "$IP" ]; then
+            GET_SVC_IP
+        else
+            curl $CURL_OPTS ${IP}:80/1;
+            [ $? -ne 0 ] && GET_SVC_IP
+        fi
+        sleep 1;
+    done
+}
+
+FROM_POD() {
+    set -x
+    kubectl run testpod --rm -it --image alpine -- sh -c "while true; do wget $WGET_OPTS -qO - web/1; sleep 1; done"
+}
+
+FROM_POD
 
