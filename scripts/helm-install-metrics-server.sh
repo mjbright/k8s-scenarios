e4e07c3843407673c9408936b8b4b3ae54984146
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-06-30 18:06:04 +0000

    Add install metrics-server script

---
+++ scripts/helm-install-metrics-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+UNINSTALL() {
+    helm uninstall -n metrics-server my-metrics-server
+}
+
+INSTALL() {
+    kubectl create ns metrics-server
+    helm repo add metrics-server https://olemarkus.github.io/metrics-server
+    #helm search repo metrics-server
+    helm install my-metrics-server -n metrics-server metrics-server/metrics-server --set args='{--kubelet-preferred-address-types=InternalIP,--kubelet-insecure-tls}'
+}
+
+WAIT_ON_METRICS() {
+    echo; echo "---- Waiting for metrics server to start"
+    while ! kubectl get --raw "/apis/metrics.k8s.io/v1beta1/nodes" ; do sleep 5; done
+
+    echo; echo "---- Waiting for first node metrics"
+    while ! kubectl top nodes; do sleep 5; done
+
+    echo; echo "---- Waiting for first pod metrics"
+    while ! kubectl top pod -A; do sleep 5; done
+}
+
+#UNINSTALL
+INSTALL
+WAIT_ON_METRICS
+
