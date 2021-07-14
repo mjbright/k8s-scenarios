e37c9fbc5f57ca6ee1598ae9d264a1f94aaa152d
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-07-14 21:23:43 +0200

    Added stackoverflow link for getting *rolebindings

--- scripts/view-rolebindings.sh
+++ scripts/view-rolebindings.sh
@@ -1,4 +1,6 @@
 
+# See: https://stackoverflow.com/questions/54602224/how-to-view-the-permissions-roles-associated-with-a-specific-service-account-in
+
 kubectl get rolebindings,clusterrolebindings -A \
     -o custom-columns='KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,SERVICE_ACCOUNTS:subjects[?(@.kind=="ServiceAccount")].name'
 
