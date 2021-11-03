2abc1f0c352e944960c641e8494123775c87248c
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-03 22:07:03 +0100

    Added install_krew.sh

---
+++ scripts/install_krew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Method as of 2021-Nov-02
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+echo 'export PATH="${PATH}:${HOME}/.krew/bin"' >> ~/.bashrc
+echo $PATH
+kubectl krew search | wc -l
+kubectl krew search
+
