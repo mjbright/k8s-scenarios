892c6428c6d75114a1545b120888561b35c49b90
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-04 20:47:21 +0100

    First attempt at using cert/keys

--- IngressNginx.TLS/curl_test.sh
+++ IngressNginx.TLS/curl_test.sh
@@ -1,10 +1,11 @@
-
+#!/bin/bash
 which jq || sudo apt-get install -y jq
 
 NODE_PORT=$( kubectl get svc my-release-ingress-nginx-controller -o json | jq '.spec.ports[0].nodePort' )
 
-curl -L -H "Host: quiz-g15.mjbright.click" 127.0.0.1:$NODE_PORT
-curl -L -H "Host: vote-g15.mjbright.click" 127.0.0.1:$NODE_PORT
-curl -L -H "Host: ingress1-g15.mjbright.click" 127.0.0.1:$NODE_PORT
+curl -L -H "Host: quiz-g15.mjbright.click" --cert certs/quiz.crt --key certs/quiz.key 127.0.0.1:$NODE_PORT
+curl -L -H "Host: vote-g15.mjbright.click" --cert certs/vote.crt --key certs/vote.key 127.0.0.1:$NODE_PORT
+curl -L -H "Host: ingress1-g15.mjbright.click" --cert certs/ingress1.crt --key certs/ingress1.key 127.0.0.1:$NODE_PORT
+
 
 
