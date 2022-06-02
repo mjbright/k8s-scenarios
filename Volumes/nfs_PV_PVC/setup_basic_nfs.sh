8213c64251828368eac056a01c796ee7be99197b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2022-06-02 11:39:31 +0200

    Added more options to nfs setup

--- Volumes/nfs_PV_PVC/setup_basic_nfs.sh
+++ Volumes/nfs_PV_PVC/setup_basic_nfs.sh
@@ -1,22 +1,60 @@
 #!/bin/bash
 
 CP=k8scp
+CP_IP=$( hostname -i )
+
 WO=worker
 
-echo; echo "---- Configuring NFS on $CP node:"
-sudo apt-get update && sudo apt-get install -y nfs-kernel-server
-sudo mkdir /opt/sfw
-sudo chmod 1777 /opt/sfw/
-sudo bash -c "echo $(date): NFS volume created from host $(hostname)"  > /opt/sfw/NFScreation.log'
-grep sfw /etc/exports || echo '/opt/sfw/ *(rw,sync,no_root_squash,subtree_check)' | sudo tee -a /etc/exports 
-grep sfw /etc/exports || echo '/opt/sfw/ *(rw,sync,no_root_squash,subtree_check)' | sudo tee -a /etc/exports 
-cat /etc/exports 
-exportfs -ra
-sudo exportfs -ra
 
-echo; echo "---- Configuring NFS on $WO node:"
-ssh $WO sudo apt-get -y install nfs-common
-ssh $WO showmount -e $CP
-ssh $WO sudo mount $CP:/opt/sfw /mnt
-ssh $WO ls -l /mnt
+MNT=/mnt/nfs-vol
+
+die() { echo "$0: die - $*" >&2; exit 1; }
+
+PRESS() {
+    echo; echo $*
+    echo "Press return>"
+    read DUMMY
+    [ "$DUMMY" = "q" ] && exit 0
+    [ "$DUMMY" = "Q" ] && exit 0
+}
+
+grep -q " $WORKER" /etc/hosts || die "No $WORKER entry found in /etc/hosts"
+
+case $(hostname) in
+    k8scp) CP=k8scp;;
+    cp) CP=cp;;
+
+    *)     die "Unexpected node name $(hostname)"
+esac
+
+ssh="ssh -i  ~/.ssh/t-key.pem $WO"
+
+
+PRESS "---- Configuring NFS on $CP node:"
+dpkg -l | grep nfs-kernel-server || {
+    echo; echo 'sudo apt-get update && sudo apt-get install -y nfs-kernel-server'
+    sudo apt-get update && sudo apt-get install -y nfs-kernel-server
+}
+
+sudo mkdir -p $MNT
+sudo chmod 1777 $MNT
+
+[ ! -f $MNT/NFScreation.log ] && {
+    echo; echo sudo tee $MNT/NFScreation.log
+    #sudo bash -c 'echo $(date): NFS volume created from host $(hostname) > $MNT/NFScreation.log'
+    bash -c "echo $(date): NFS volume created from host $(hostname)" | sudo tee $MNT/NFScreation.log
+}
+
+grep $MNT /etc/exports || {
+    echo $MNT/ '*(rw,sync,no_root_squash,subtree_check)' | sudo tee -a /etc/exports 
+    #cat /etc/exports 
+    # Reload exports:
+    sudo exportfs -ra
+}
+
+PRESS "---- Configuring NFS on $WO node:"
+$ssh sudo dpkg -l | grep nfs-common || $ssh sudo apt-get -y install nfs-common
+$ssh showmount -e $CP_IP
+$ssh sudo mount $CP_IP:$MNT /mnt
+$ssh ls -l /mnt
 
