2f95e70d35e5a25fc102dc29b62cb389a3e43152
Michael Bright <1880109+mjbright@users.noreply.github.com>
2022-06-21 14:02:16 +0000

    Code improvements to metrics-server install

--- scripts/helm-install-metrics-server.sh
+++ scripts/helm-install-metrics-server.sh
@@ -4,8 +4,11 @@ die() { echo "$0: die - $*" >&2; exit 1; }
 
 which helm >/dev/null || die "You need to install helm first"
 
+#NS="monitoring"
+NS="metrics-server"
+
 UNINSTALL() {
-    helm uninstall -n metrics-server my-metrics-server
+    helm uninstall -n $NS my-metrics-server
 }
 
 INSTALL() {
@@ -13,7 +16,7 @@ INSTALL() {
     # helm repo add metrics-server https://olemarkus.github.io/metrics-server
     helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
     helm search repo metrics-server
-    helm install my-metrics-server -n metrics-server metrics-server/metrics-server --set args='{--kubelet-preferred-address-types=InternalIP,--kubelet-insecure-tls}'
+    helm install my-metrics-server -n $NS metrics-server/metrics-server --set args='{--kubelet-preferred-address-types=InternalIP,--kubelet-insecure-tls}'
 }
 
 WAIT_ON_METRICS() {
@@ -27,7 +30,20 @@ WAIT_ON_METRICS() {
     while ! kubectl top pod -A; do sleep 5; done
 }
 
-#UNINSTALL
+kubectl get ns $NS || kubectl create ns $NS
+
+if [ "$1" = "-d" ]; then
+    UNINSTALL
+    exit $?
+fi
+
 INSTALL
 WAIT_ON_METRICS
+
+set -x
+kubectl get --raw "/apis/metrics.k8s.io/v1beta1/nodes" | jq '.items[].metadata.name'
+set +x
+#kubectl get --raw "/apis/metrics.k8s.io/v1beta1/pods" | jq '.items[].metadata.name'
+#kubectl get --raw "/apis/metrics.k8s.io/v1beta1/namespaces/default/pods" | jq '.items[].metadata.name'
+
 
