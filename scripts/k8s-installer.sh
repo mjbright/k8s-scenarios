5cb13a7f5c9a5cbac82a55c29d9330da7e60d0f5
Michael Bright <1880109+mjbright@users.noreply.github.com>
2022-11-13 17:28:50 +0000

    Corrected (podman) rsync command

--- scripts/k8s-installer.sh
+++ scripts/k8s-installer.sh
@@ -513,9 +513,9 @@ INSTALL_PODMAN() {
     RUN wget $PODMAN_URL
     RUN tar xf podman-linux-amd64.tar.gz
 
-    #RUN sudo rsync -anv ~/tmp/podman-linux-amd64/ /
-    RUN sudo rsync -anv ~/tmp/podman-linux-amd64/usr/ /usr/
-    RUN sudo rsync -anv ~/tmp/podman-linux-amd64/etc/ /etc/
+    #RUN sudo rsync -av ~/tmp/podman-linux-amd64/ /
+    RUN sudo rsync -av ~/tmp/podman-linux-amd64/usr/ /usr/
+    RUN sudo rsync -av ~/tmp/podman-linux-amd64/etc/ /etc/
     #RUN sudo cp -a podman-linux-amd64/usr/local/bin/podman /usr/local/bin
     #RUN sudo cp -a podman-linux-amd64/usr/local/lib/*      /usr/local/lib/
 
