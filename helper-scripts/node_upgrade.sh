d9d68086398b2d6f5eae631b43d8a01672be1334
Michael Bright <1880109+mjbright@users.noreply.github.com>
2022-12-07 20:34:30 +0000

    Made script more flexible/robust

--- helper-scripts/node_upgrade.sh
+++ helper-scripts/node_upgrade.sh
@@ -8,9 +8,11 @@
 #
 
 NODE2=worker
-HOST=$(hostname)
+HOSTNAME=$(hostname)
 
 RELEASE=1.25.4
+
+## Funcs: ----------------------------------------------------------
 
 die()   { echo "$0: die - $*" >&2; exit 1; }
 
@@ -29,14 +31,6 @@ RUN()   {
 
     [ $RET -ne 0 ] && echo "Error: Command returned $RET"
 }
-
-case $HOSTNAME in
-  *cp)    NODE_NAME=$( kubectl get nodes -o name | grep -m1 cp | sed 's?.*/??' );;
-  *work*) die "Must be run from 'cp' Node";; # NODE_NAME=$( kubectl get nodes -o name | grep -m1 work | sed 's?.*/??' );;
-  *)      die "Unrecognised node name"
-esac
-
-ssh $NODE2 uptime || die "ssh access to '$NODE2' must be enabled"
 
 UPGRADE_NODE() {
     NODE_NAME="$1"; shift;
@@ -66,6 +60,42 @@ UPGRADE_NODE() {
     RUN kubectl get node
 }
 
-UPGRADE_NODE cp     ""
-UPGRADE_NODE $NODE2 "ssh -t $NODE2"
+DEFAULT_ACTION_2_NODE_UPGRADE() {
+    # Upgrade this (cp?) Node:
+    UPGRADE_NODE $NODE_NAME     ""
+
+    # Upgrade second (worker?) Node: connect via ssh
+    UPGRADE_NODE $NODE2         "ssh -t $NODE2"
+}
+
+## Args: ----------------------------------------------------------
+
+# Determine on what node we are running - a cp or worker node ?
+
+case $HOSTNAME in
+  *cp)    NODE_NAME=$( kubectl get nodes -o name | grep -m1 cp | sed 's?.*/??' );;
+  *work*) die "Must be run from 'cp' Node";; # NODE_NAME=$( kubectl get nodes -o name | grep -m1 work | sed 's?.*/??' );;
+  *)      die "Unrecognised node name '$HOSTNAME'"
+esac
+
+while [ ! -z "$1" ]; do
+    case $1 in
+        -n) shift; NODE=$1; 
+            ssh $NODE uptime ||
+                die "ssh access to '$NODE' must be enabled"
+
+            UPGRADE_NODE $NODE2         "ssh -t $NODE2"
+            exit $?
+            ;;
+
+         *) die "Unknown option '$1'";;
+    esac
+done
+
+## Main: ----------------------------------------------------------
+
+ssh $NODE2 uptime ||
+    die "ssh access to '$NODE2' must be enabled"
+
+DEFAULT_ACTION_2_NODE_UPGRADE
 
