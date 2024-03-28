c7e00658ab0d54b51f4ab8254300d902f8d9e371
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-03-28 21:17:47 +0100

    Added missing -y on apt install

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -41,7 +41,7 @@ ADD_DOCKER_REPO() {
 
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg
+    sudo apt-get install -y ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
